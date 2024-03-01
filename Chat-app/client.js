17a96aaa873da94a2484aa2ba84fd9db3979630c
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2024-03-01 14:15:14 +0530

    experimenting on chat room messaging

--- Chat-app/client.js
+++ Chat-app/client.js
@@ -1,7 +1,27 @@
 const net = require('net')
+const readlinePromises = require('node:readline/promises');
+
+// Reading the userInput from console
+const rl = readlinePromises.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+}); 
+
+
 
 // Here we are connecting to the server
-const client = net.createConnection({host:"127.0.0.1",port:3000},()=>{
+const socket = net.createConnection({host:"127.0.0.1",port:3000},async()=>{
     console.log('Connected to server')
+    const message = await rl.question("Enter a Message > ")
+    
+    socket.write(message)
+    
 })
 
+socket.on('data',(data)=>{
+    console.log(data.toString('utf-8'))
+})
+
+socket.on('end',()=>{
+    console.log(`connection Ended`)
+})
--- Chat-app/server.js
+++ Chat-app/server.js
@@ -2,13 +2,20 @@ const net = require('net')
 
 const port  = 3000
 const host = "127.0.0.1"
+const clients =[]
 
 // net.createServer is used to create TCP/IPC server
 const server = net.createServer()
 
+
 // When client connects to this server connection event is emitted and it is consumed here.
-server.on('connection',(socket)=>{
+server.on('connection',(socket)=>{             /* Socket -  it's a Duplex Stream */
     console.log('New connection established to server')
+    socket.on("data",(data)=>{
+        // console.log(data.toString("utf-8"))
+        clients.map((s)=>s.write(data))
+        clients.push(socket)
+    })
 })
 
 // server is listening on port:3000 
